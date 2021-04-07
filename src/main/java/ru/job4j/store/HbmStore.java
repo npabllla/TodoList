5912f5449d6ac27eafca145e104c537162600f53
Kirill <npabllla@gmail.com>
2021-04-07 20:09:09 +0300

    3. Лямбды и шаблон wrapper. [#492577]

--- src/main/java/ru/job4j/store/HbmStore.java
+++ src/main/java/ru/job4j/store/HbmStore.java
@@ -2,6 +2,7 @@ package ru.job4j.store;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
@@ -10,11 +11,12 @@ import ru.job4j.model.Item;
 import javax.persistence.Query;
 import java.util.Collection;
 import java.util.List;
+import java.util.function.Function;
 
 public class HbmStore implements Store {
     private final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
             .configure().build();
-    private final SessionFactory sf = new MetadataSources(registry)
+    private final SessionFactory factory = new MetadataSources(registry)
             .buildMetadata().buildSessionFactory();
 
     private static final class Lazy {
@@ -27,31 +29,39 @@ public class HbmStore implements Store {
 
     @Override
     public void save(Item item) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        session.save(item);
-        session.getTransaction().commit();
-        session.close();
+        transaction(session -> session.save(item));
     }
 
     @Override
     public void update(int id) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        Query query = session.createQuery("update Item set status='Done' where id=:id");
-        query.setParameter("id", id);
-        query.executeUpdate();
-        session.getTransaction().commit();
-        session.close();
+        transaction(session -> {
+                final Query query = session.createQuery("update Item set status='Done' where id=:id");
+                query.setParameter("id", id);
+                query.executeUpdate();
+                return null;
+            }
+        );
     }
 
     @Override
     public Collection<Item> findAll() {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        List<Item> result = (List<Item>) session.createQuery("from ru.job4j.model.Item").list();
-        session.getTransaction().commit();
-        session.close();
-        return result;
+        return  (List<Item>) transaction(
+                session1 -> session1.createQuery("from Item").list()
+        );
+    }
+
+    private <T> T transaction(final Function<Session, T> command) {
+        final Session session = factory.openSession();
+        final Transaction tx = session.beginTransaction();
+        try {
+            T rsl = command.apply(session);
+            tx.commit();
+            return rsl;
+        } catch (final Exception e) {
+            session.getTransaction().rollback();
+            throw e;
+        } finally {
+            session.close();
+        }
     }
 }
